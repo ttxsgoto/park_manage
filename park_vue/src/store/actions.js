1fc3540127794c3518fe0f3389509c58419697ae
zhongqiang <zhongqiang>
2019-05-17 07:41:21 +0800

    new:vuex

---
+++ park_vue/src/store/actions.js
@@ -0,0 +1,23 @@
+// set data to vuex and get data
+//set
+// import {mapMutations, mapGetters} from 'vuex'
+//
+// methods: {
+//     // 添加数据
+//     this.setMember('xx')
+//     ...mapMutations({
+//         setMember: 'SET_MEMBER'
+//     })
+//     // 取数据
+//
+//     computed:{
+//         ...mapGetters([
+//             'member'
+//         ])
+//     },
+//     create(){
+//         console.log(this.member)
+//     }
+// }
+
+
